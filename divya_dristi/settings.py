e4a1a7cb192d47bc91e6af29410f9758ae0bef0a
Avinash Bharti <abharti1111@gmail.com>
2020-05-19 01:33:01 +0530

    fixed settings file

--- divya_dristi/settings.py
+++ divya_dristi/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'divya_dristi',
         'USER': 'dbuser',
-        'PASSWORD': 'Avinash@99',
+        'PASSWORD': '***************',
         'HOST': 'localhost',
         'PORT': '5432',
     }
